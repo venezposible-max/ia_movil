71a85b7f7896f40b871ab2128117a4a8b8bbdbca
infogadgets2024 <infogadgets2024@gmail.com>
2026-02-07 21:14:46 -0400

    Fix: Force Env Vars in Vite Config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
+export default defineConfig(({ mode }) => {
+    // Cargar variables de entorno (por si estamos en local)
+    const env = loadEnv(mode, process.cwd(), '');
+    return {
+        plugins: [react()],
+        define: {
+            'import.meta.env.VITE_GROQ_API_KEY': JSON.stringify(process.env.VITE_GROQ_API_KEY || env.VITE_GROQ_API_KEY),
+            'import.meta.env.VITE_SERPER_API_KEY': JSON.stringify(process.env.VITE_SERPER_API_KEY || env.VITE_SERPER_API_KEY),
+        }
+    }
 })
